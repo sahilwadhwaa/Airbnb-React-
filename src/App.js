dab95fc5012832c89a3fdafb24958a68b9b84c70
sahilwadhwaa <104511479+sahilwadhwaa@users.noreply.github.com>
2022-09-19 13:10:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./components/data"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    const cards= data.map(item=>{
+        return(
+            <Card
+                key={item.id}
+                item={item}
+            />    
+        )
+    })
+    return (
+        <div>
+          <Navbar />
+          <Hero />
+          <section className="cards-list">
+                {cards}
+            </section>
+          
+        </div>
+    )
+}
