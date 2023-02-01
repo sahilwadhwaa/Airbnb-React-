3544ffc7fad07d63bab7d094cf9c260dabe3e4cb
sahilwadhwaa <sahilwadhwa.5454@gmail.com>
2023-02-01 16:28:16 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b7195bbc38f4b0491be3887c4c463214.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5132215570d228fae690",
+    "url": "/React-Airbnb/static/js/main.51322155.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/React-Airbnb/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "a7579b779aa675eb2864",
+    "url": "/React-Airbnb/static/js/runtime~main.a7579b77.js"
+  },
+  {
+    "revision": "2fe0c7c3615f334f0ed748238843d5eb",
+    "url": "/React-Airbnb/index.html"
+  }
+];
